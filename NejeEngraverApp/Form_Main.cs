58c79d29327462f5b48b9af63ebcc09ced46db3e
Andrey B. Malyshenko <andrey.malyshenko@gmail.com>
2019-01-29 20:38:32 +0100

    fixed missing event handler

--- NejeEngraverApp/Form_Main.cs
+++ NejeEngraverApp/Form_Main.cs
@@ -3958,7 +3958,6 @@ namespace NejeEngraverApp
             this.pictureBox1.Size = new System.Drawing.Size(502, 502);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.MouseDown += new MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new MouseEventHandler(this.pictureBox1_MouseUp);
